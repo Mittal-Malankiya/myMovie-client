e9eb24fdde8b20edd1687772aaed3cb2d721f76b
Mittal-Malankiya <m4mittal19@gmail.com>
2024-02-29 15:03:55 +0100

    update file and folders

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,64 +1,77 @@
-import React from "react";
 import { useState } from "react";
-import { BookCard } from "../book-card/book-card";
-import { BookView } from "../book-view/book-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [books, setBooks] = useState([
+  const [movies, setMovies] = useState([
     {
       id: 1,
-      title: "Eloquent JavaScript",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Marijn Haverbeke",
+      title: "Infinite Dreams",
+      image: "https://m.media-amazon.com/images/I/81GsozHQ86L._SY466_.jpg",
+      genre: "Horror",
+      director: "James Wan",
+      description:
+        "Another terrifying horror film from James Wan that will haunt your dreams.",
+      bio: "James Wan is a Malaysian-Australian film director, screenwriter, and producer. He is best known for his work in the horror genre.",
     },
     {
       id: 2,
-      title: "Mastering JavaScript Functional Programming",
+      title: "Whispers in the Dark",
       image:
-        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-      author: "Federico Kereki",
+        "https://m.media-amazon.com/images/I/516TEgcmF4L._SY445_SX342_.jpg",
+      genre: "Fantasy",
+      director: "Guillermo del Toro",
+      description:
+        '"The Enchanted Kingdom," embark on a magical journey to a world where dragons soar through the skies, ancient spells come to life, and mystical creatures inhabit enchanted forests. Follow the epic quest of a young hero as they discover their hidden powers and join forces with a fellowship of wizards, elves, and talking animals.',
+      bio: "Guillermo del Torois a Malaysian-Australian film director, screenwriter, and producer. He is best known for his work in the horror genre.",
     },
     {
       id: 3,
-      title: "JavaScript: The Good Parts",
+      title: "Beyond the Horizon",
       image:
-        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-      author: "Douglas Crockford",
+        "https://m.media-amazon.com/images/I/41v1e4oMpSL._SY445_SX342_.jpg",
+      genre: "Thriller",
+      director: "David Fincher",
+      description:
+        "A pulse-pounding thriller that will keep you guessing until the very end.",
+      bio: "David Fincher is an American film director and producer. He is known for his dark and stylish thrillers, including 'Se7en' and 'Fight Club'.",
     },
     {
       id: 4,
-      title: "JavaScript: The Definitive Guide",
+      title: "Shadow of the Past",
       image:
-        "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-      author: "David Flanagan",
-    },
-    {
-      id: 5,
-      title: "The Road to React",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
-      author: "Robin Wieruch",
+        "https://m.media-amazon.com/images/I/51WuRWAAbuL._SY445_SX342_.jpg",
+      genre: "Adventure",
+      director: "Peter Jackson",
+      description:
+        "A thrilling adventure set in a fantastical world filled with danger and wonder.",
+      bio: "Peter Jackson is a New Zealand film director, producer, and screenwriter. He is best known for directing the 'Lord of the Rings' film trilogy.",
     },
   ]);
-  if (books.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-  const [selectedBook, setSelectedBook] = useState(null);
 
-  if (selectedBook) {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
     return (
-      <BookView book={selectedBook} onBackClick={() => setSelectedBook(null)} />
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
     );
   }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
   return (
     <div>
-      {books.map((book) => (
-        <BookCard
-          key={book.id}
-          book={book}
-          onBookClick={(newSelectedBook) => {
-            setSelectedBook(newSelectedBook);
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
           }}
         />
       ))}
