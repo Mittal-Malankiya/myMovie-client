4d7365d61e82e3e13c69a466d23a58c8c43b436c
Mittal-Malankiya <m4mittal19@gmail.com>
2024-03-07 11:48:13 +0100

    update index.js

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,42 +1,70 @@
 import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} alt={movie.title} />
+        <img src={movie.banana} alt={movie.title} />
       </div>
       <div>
-        <span>Title: </span>
+        <span>
+          <strong>Title:</strong>
+        </span>
         <span>{movie.title}</span>
       </div>
       <div>
-        <span>Genre: </span>
+        <span>
+          <strong>Genre: </strong>
+        </span>
         <span>{movie.genre}</span>
       </div>
       <div>
-        <span>Director: </span>
+        <span>
+          <strong>Director:</strong>
+        </span>
         <span>{movie.director}</span>
       </div>
       <div>
-        <span>Description: </span>
+        <span>
+          <strong>Description:</strong>{" "}
+        </span>
         <span>{movie.description}</span>
       </div>
       <div>
-        <span>Bio: </span>
+        <span>
+          <strong>Bio: </strong>
+        </span>
         <span>{movie.bio}</span>
       </div>
-      <button onClick={onBackClick}>Back</button>
+      <>
+        <style type="text/css">
+          {`
+           .btn-flat {
+            background-color: #228B22;
+            color: white;
+                     }
+            .btn-xxl {
+             padding: 1rem 1.5rem;
+              font-size: 1.5rem;
+            }
+         `}
+        </style>
+        <Button onClick={onBackClick} variant="flat" size="md">
+          Back{" "}
+        </Button>
+      </>
     </div>
   );
 };
 MovieView.propTypes = {
   movie: PropTypes.shape({
-    movieid: PropTypes.string,
+    _id: PropTypes.string,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     genre: PropTypes.string,
     director: PropTypes.string,
     bio: PropTypes.string,
+    banana: PropTypes.string.isRequired,
   }).isRequired,
   onBackClick: PropTypes.func.isRequired,
 };
